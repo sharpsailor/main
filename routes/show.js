f84c99ccfab439c8be92de2d3bb9b839a5ba47c2
sharpsailor <sharpsailor13@gmail.com>
2022-10-29 00:25:21 +0530

    download

--- routes/show.js
+++ routes/show.js
@@ -2,18 +2,23 @@ const router = require("express").Router();
 const File = require("../models/file");
 router.get("/:uuid", async (req, res) => {
   try {
-    const file = await File.findOne({ uuid: req.params.uuid });
-    if(!file){
-        return res.render("download", { error: "Link has been expired" });
+    let downloadLink;
+    if (process.env.APP_BASE_URL.endsWith("/")) {
+      downloadLink = `${process.env.APP_BASE_URL}files/download/${file.uuid}`;
+    } else {
+      downloadLink = `${process.env.APP_BASE_URL}/files/download/${file.uuid}`;
     }
-    
-    return res.render('download',{
-        uuid:file.uuid,
-        fileName:file.filename,
-        fileSize:file.size,
-        downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-        
-    })
+    const file = await File.findOne({ uuid: req.params.uuid });
+    if (!file) {
+      return res.render("download", { error: "Link has been expired" });
+    }
+
+    return res.render("download", {
+      uuid: file.uuid,
+      fileName: file.filename,
+      fileSize: file.size,
+      downloadLink,
+    });
   } catch (err) {
     return res.render("download", { error: "Something went wrong" });
   }
