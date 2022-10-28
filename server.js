30e1c13a05e1b99646bf2bc2dbc7232c5dabad78
sharpsailor <sharpsailor13@gmail.com>
2022-10-28 10:31:43 +0530

    template engine

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 const connectDB = require("./config/db");
 connectDB();
+// Template engine
+app.set("views", path.join(__dirname, "/views"));
+app.set('view engine','ejs');
 // Routes
-app.use('/api/files', require('./routes/file'));
+app.use("/api/files", require("./routes/file"));
+app.use("/files", require("./routes/show"));
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
