002f4561bd717c347fd84ef157f690e837244fa5
sharpsailor <sharpsailor13@gmail.com>
2022-10-28 22:02:21 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
 const connectDB = require("./config/db");
 connectDB();
+
+// cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(","),
+  // ['http://localhost:3000','http://localhost:3300','http://localhost:5000']
+};
+app.use(cors(corsOptions));
 // Template engines
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
