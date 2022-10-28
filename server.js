bd45d9a9d8b4c0cc0a2a102ddadd06976cb343bf
sharpsailor <sharpsailor13@gmail.com>
2022-10-28 12:19:10 +0530

    Download Link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 // Routes
-app.use("/api/files", require("./routes/file"));
+app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
 app.use("/files/download", require("./routes/download"));
 app.listen(PORT, () => {
